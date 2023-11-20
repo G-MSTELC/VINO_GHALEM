0d54c428e0ee440613e6bf0b9d4d00e7cf5101d6
G-MSTELC <ghanem.telc@gmail.com>
2023-11-20 15:05:26 -0500

    Integration des options de filtrage

--- app/Http/Controllers/BouteilleCellierController.php
+++ app/Http/Controllers/BouteilleCellierController.php
@@ -73,62 +73,73 @@ class BouteilleCellierController extends Controller
         return redirect(route('cellier.show', $cellier_id));
     }
     // fonction recherche & filtrage
-
     public function rechercheEtFiltrage(Request $request, $cellier_id)
-{
-    $cellier = Cellier::findOrFail($cellier_id);
-
-    $nomBouteille = $request->input('keyword');
-    $typeVin = $request->input('filtrage');
-    $anneeVin = $request->filled('annee_vin') ? $request->input('annee_vin') : null;
-    $paysVin = $request->input('pays');
-    $imageVin = $request->input('image');
-    $sort = $request->input('sort', ''); 
-
-    $bouteilleQuery = $cellier->bouteillesCelliers()
-        ->when($nomBouteille, function ($query) use ($nomBouteille) {
-            $query->whereHas('bouteille', function ($subquery) use ($nomBouteille) {
-                $subquery->where('nom', 'like', "%$nomBouteille%")
-                    ->orWhere('type', 'like', "%$nomBouteille%"); 
+    {
+        $cellier = Cellier::findOrFail($cellier_id);
+    
+        $nomBouteille = $request->input('keyword');
+        $typeVin = $request->input('filtrage');
+        $anneeVin = $request->filled('annee_vin') ? $request->input('annee_vin') : null;
+        $paysVin = $request->input('pays');
+        $imageVin = $request->input('image');
+        $prixMin = $request->filled('prix_min') ? $request->input('prix_min') : null;
+        $prixMax = $request->filled('prix_max') ? $request->input('prix_max') : null;
+        $sort = $request->input('sort', '');
+    
+        $bouteilleQuery = $cellier->bouteillesCelliers()
+            ->when($nomBouteille, function ($query) use ($nomBouteille) {
+                $query->whereHas('bouteille', function ($subquery) use ($nomBouteille) {
+                    $subquery->where('nom', 'like', "$nomBouteille%")
+                        ->orWhere('type', 'like', "$nomBouteille%");
+                });
+            })
+            ->when($typeVin, function ($query) use ($typeVin) {
+                $query->whereHas('bouteille', function ($subquery) use ($typeVin) {
+                    $subquery->where('type', $typeVin);
+                });
+            })
+            ->when($anneeVin !== null, function ($query) use ($anneeVin) {
+                $query->whereHas('bouteille', function ($subquery) use ($anneeVin) {
+                    $subquery->where('annee', $anneeVin);
+                });
+            })
+            ->when($paysVin, function ($query) use ($paysVin) {
+                $query->whereHas('bouteille', function ($subquery) use ($paysVin) {
+                    $subquery->where('pays', $paysVin);
+                });
+            })
+            ->when($imageVin !== null, function ($query) use ($imageVin) {
+                $query->whereHas('bouteille', function ($subquery) use ($imageVin) {
+                    $subquery->where('srcImage', $imageVin ? '!=' : '=', null);
+                });
+            })
+            ->when($prixMin, function ($query) use ($prixMin) {
+                $query->whereHas('bouteille', function ($subquery) use ($prixMin) {
+                    $subquery->where('prix', '>=', $prixMin);
+                });
+            })
+            ->when($prixMax, function ($query) use ($prixMax) {
+                $query->whereHas('bouteille', function ($subquery) use ($prixMax) {
+                    $subquery->where('prix', '<=', $prixMax);
+                });
+            })
+            ->when($sort, function ($query) use ($sort) {
+                if ($sort == 'name-asc') {
+                    $query->orderBy('bouteille.nom');
+                } elseif ($sort == 'name-desc') {
+                    $query->orderByDesc('bouteille.nom');
+                } elseif ($sort == 'price-asc') {
+                    $query->orderBy('bouteille.prix');
+                } elseif ($sort == 'price-desc') {
+                    $query->orderByDesc('bouteille.prix');
+                }
             });
-        })
-        ->when($typeVin, function ($query) use ($typeVin) {
-            $query->whereHas('bouteille', function ($subquery) use ($typeVin) {
-                $subquery->where('type', $typeVin);
-            });
-        })
-        ->when($anneeVin !== null, function ($query) use ($anneeVin) {
-            $query->whereHas('bouteille', function ($subquery) use ($anneeVin) {
-                $subquery->where('annee', $anneeVin);
-            });
-        })
-        ->when($paysVin, function ($query) use ($paysVin) {
-            $query->whereHas('bouteille', function ($subquery) use ($paysVin) {
-                $subquery->where('pays', $paysVin);
-            });
-        })
-        ->when($imageVin !== null, function ($query) use ($imageVin) {
-            $query->whereHas('bouteille', function ($subquery) use ($imageVin) {
-                $subquery->where('image', $imageVin ? '!=' : '=', null);
-            });
-        })
-        ->when($sort, function ($query) use ($sort) {
-            if ($sort == 'name-asc') {
-                $query->orderBy('bouteille.nom');
-            } elseif ($sort == 'name-desc') {
-                $query->orderByDesc('bouteille.nom');
-            } elseif ($sort == 'price-asc') {
-                $query->orderBy('bouteille.prix');
-            } elseif ($sort == 'price-desc') {
-                $query->orderByDesc('bouteille.prix');
-            }
-        });
-
-    $bouteilles = $bouteilleQuery->get();
-
-    return view('cellier.show-search', compact('cellier', 'bouteilles'));
-}
-
+    
+        $bouteilles = $bouteilleQuery->get();
+    
+        return view('cellier.show-search', compact('cellier', 'bouteilles'));
+    }
+    
 
 
 }
--- resources/views/cellier/recherche.blade.php
+++ resources/views/cellier/recherche.blade.php
@@ -6,11 +6,9 @@
 @section('content')
     <header>
         @isset($cellier)
-            <a href="{{ route('cellier.show', ['cellier_id' => $cellier->id]) }}" class="btn-arrow-top">
-            </a>
+            <a href="{{ route('cellier.show', ['cellier_id' => $cellier->id]) }}" class="btn-arrow-top"></a>
         @else
-            <a href="{{ route('cellier.index') }}" class="btn-arrow-top">
-            </a>
+            <a href="{{ route('cellier.index') }}" class="btn-arrow-top"></a>
             <a href="{{ route('cellier.create') }}" class="btn btn-success">
                 <i class="fas fa-plus"></i> Ajouter un cellier
             </a>
@@ -19,46 +17,62 @@
     <main class="nav-margin">
         <h1>Résultats de la recherche</h1>
 
-        <!-- Formulaire de recherche par mot-clé -->
+        <!-- Formulaire de recherche par mot-clé, phrase ou nom de bouteille -->
         <form action="{{ route('rechercheEtFiltrage.cellier', ['cellier_id' => $cellier->id]) }}" method="post" class="form-inline">
             @csrf
-            <label for="keyword" class="sr-only">Recherche par mot-clé :</label>
-            <input type="text" name="keyword" id="keyword" class="form-control mb-2 mr-sm-2" placeholder="Recherche par mot-clé" value="{{ request('keyword') }}">
-            <button type="submit" class="btn btn-primary mb-2">Rechercher</button>
-        </form>
+            <label for="keyword" class="sr-only">Recherche par mot-clé, phrase ou nom de bouteille :</label>
+            <input type="text" name="keyword" id="keyword" class="form-control mb-2 mr-sm-2" placeholder="Recherche par mot-clé, phrase ou nom de bouteille" value="{{ request('keyword') }}">
+            
+            <!-- Formulaire de filtrage -->
+            <div class="form-group">
+                <label for="filtrage" class="sr-only">Filtrer par type de vin :</label>
+                <select name="filtrage" id="filtrage" class="custom-select mb-2 mr-sm-2">
+                    <option value="" {{ request('filtrage') == '' ? 'selected' : '' }}>Tous les types</option>
+                    <option value="rouge" {{ request('filtrage') == 'rouge' ? 'selected' : '' }}>Vin Rouge</option>
+                    <option value="blanc" {{ request('filtrage') == 'blanc' ? 'selected' : '' }}>Vin Blanc</option>
+                </select>
+            </div>
 
-        <!-- Formulaire de filtrage -->
-        <form action="{{ route('rechercheEtFiltrage.cellier', ['cellier_id' => $cellier->id]) }}" method="post" class="form-inline">
-            @csrf
+            <div class="form-group">
+                <label for="region" class="sr-only">Filtrer par région :</label>
+                <input type="text" name="region" id="region" class="form-control mb-2 mr-sm-2" placeholder="Entrez la région" value="{{ request('region') }}">
+            </div>
 
-            <label for="filtrage" class="sr-only">Filtrer par type de vin :</label>
-            <select name="filtrage" id="filtrage" class="custom-select mb-2 mr-sm-2">
-                <option value="" {{ request('filtrage') == '' ? 'selected' : '' }}>Tous les types</option>
-                <option value="rouge" {{ request('filtrage') == 'rouge' ? 'selected' : '' }}>Vin Rouge</option>
-                <option value="blanc" {{ request('filtrage') == 'blanc' ? 'selected' : '' }}>Vin Blanc</option>
-            </select>
+            <div class="form-group">
+                <label for="pays" class="sr-only">Filtrer par pays :</label>
+                <select name="pays" id="pays" class="custom-select mb-2 mr-sm-2">
+                    
+                </select>
+            </div>
 
-            <label for="image" class="sr-only">Filtrer par image :</label>
-            <select name="image" id="image" class="custom-select mb-2 mr-sm-2">
-                <option value="" {{ request('image') == '' ? 'selected' : '' }}>Toutes les images</option>
-                <option value="1" {{ request('image') == '1' ? 'selected' : '' }}>Avec image</option>
-                <option value="0" {{ request('image') == '0' ? 'selected' : '' }}>Sans image</option>
-            </select>
+            <div class="form-group">
+                <label for="annee_vin" class="sr-only">Année (facultatif) :</label>
+                <input type="number" name="annee_vin" class="form-control mb-2 mr-sm-2" placeholder="Année (laisser vide pour ignorer)" value="{{ request('annee_vin') }}">
+            </div>
 
-            <label for="pays" class="sr-only">Filtrer par pays :</label>
-            <select name="pays" id="pays" class="custom-select mb-2 mr-sm-2">
-                <option value="" {{ request('pays') == '' ? 'selected' : '' }}>Tous les pays</option>
-                
-            </select>
+            <div class="form-group">
+                <label for="image" class="sr-only">Filtrer par image :</label>
+                <select name="image" id="image" class="custom-select mb-2 mr-sm-2">
+                    <option value="" {{ request('image') == '' ? 'selected' : '' }}>Toutes les images</option>
+                    <option value="1" {{ request('image') == '1' ? 'selected' : '' }}>Avec image</option>
+                    <option value="0" {{ request('image') == '0' ? 'selected' : '' }}>Sans image</option>
+                </select>
+            </div>
 
-            <label for="annee_vin" class="sr-only">Année (alternative) :</label>
-            <input type="number" name="annee_vin" class="form-control mb-2 mr-sm-2" placeholder="Année (laisser vide pour ignorer)" value="{{ request('annee_vin') }}">
+            <div class="form-group">
+                <label for="prix_min" class="sr-only">Prix minimum :</label>
+                <input type="number" name="prix_min" class="form-control mb-2 mr-sm-2" placeholder="Prix minimum" value="{{ request('prix_min') }}">
+            </div>
+
+            <div class="form-group">
+                <label for="prix_max" class="sr-only">Prix maximum :</label>
+                <input type="number" name="prix_max" class="form-control mb-2 mr-sm-2" placeholder="Prix maximum" value="{{ request('prix_max') }}">
+            </div>
 
             <button type="button" class="btn btn-primary mb-2 mr-sm-2" data-toggle="collapse" data-target="#collapseFiltrage" aria-expanded="false" aria-controls="collapseFiltrage">
                 Filtrage
             </button>
 
-        
             <div class="collapse" id="collapseFiltrage">
                 
             </div>
