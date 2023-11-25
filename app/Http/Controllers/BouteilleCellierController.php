4213b34d7a1f65aff85d7c67176192894a1e595d
G-MSTELC <ghanem.telc@gmail.com>
2023-11-25 17:20:56 -0500

    Ajout d'améliorations CSS sur la page de résultats de recherche et intégration d'un script pour le bouton de filtrage. Les tests sont concluants.

--- app/Http/Controllers/BouteilleCellierController.php
+++ app/Http/Controllers/BouteilleCellierController.php
@@ -73,95 +73,96 @@ class BouteilleCellierController extends Controller
     
     //fonction recherche & filtrage
     public function rechercheEtFiltrage(Request $request, $cellier_id)
-{
-    $cellier = Cellier::findOrFail($cellier_id);
-
-    $nomBouteille = $request->input('keyword');
-    $typeVin = $request->input('type');
-    $anneeVin = $request->filled('annee_vin') ? $request->input('annee_vin') : null;
-    $paysVin = $request->input('pays');
-    $regionVin = $request->input('region');
-    $imageVin = $request->input('image');
-    $prixMin = $request->filled('prix_min') ? $request->input('prix_min') : null;
-    $prixMax = $request->filled('prix_max') ? $request->input('prix_max') : null;
-    $tauxSucre = $request->input('tauxSucre');
-    $degre = $request->input('degre');
-    $sort = $request->input('sort', '');
-
-    $bouteilleQuery = $cellier->bouteillesCelliers()
-        ->when($nomBouteille, function ($query) use ($nomBouteille) {
-            $query->whereHas('bouteille', function ($subquery) use ($nomBouteille) {
-                $subquery->where('nom', 'like', "$nomBouteille%")
-                    ->orWhere('type', 'like', "$nomBouteille%");
-            });
-        })
-        ->when($typeVin, function ($query) use ($typeVin) {
-            if ($typeVin !== '') {
-                $query->whereHas('bouteille', function ($subquery) use ($typeVin) {
-                    $subquery->where('type', 'like', "%$typeVin%");
+    {
+        $cellier = Cellier::findOrFail($cellier_id);
+    
+        $nomBouteille = $request->input('keyword');
+        $typeVin = $request->input('type');
+        $anneeVin = $request->filled('annee_vin') ? $request->input('annee_vin') : null;
+        $paysVin = $request->input('pays');
+        $regionVin = $request->input('region');
+        $imageVin = $request->input('image');
+        $prixMin = $request->filled('prix_min') ? $request->input('prix_min') : null;
+        $prixMax = $request->filled('prix_max') ? $request->input('prix_max') : null;
+        $tauxSucre = $request->input('tauxSucre');
+        $degre = $request->input('degre');
+        $sort = $request->input('sort', '');
+    
+        $bouteilleQuery = $cellier->bouteillesCelliers()
+            ->when($nomBouteille, function ($query) use ($nomBouteille) {
+                $query->whereHas('bouteille', function ($subquery) use ($nomBouteille) {
+                    $subquery->where('nom', 'like', "$nomBouteille%")
+                        ->orWhere('type', 'like', "$nomBouteille%");
                 });
-            }
-        })
-        ->when($anneeVin !== null, function ($query) use ($anneeVin) {
-            $query->whereHas('bouteille', function ($subquery) use ($anneeVin) {
-                $subquery->where('annee', $anneeVin);
-            });
-        })
-        ->when($paysVin !== null, function ($query) use ($paysVin) {
-            $query->whereHas('bouteille', function ($subquery) use ($paysVin) {
-                $subquery->where('pays', $paysVin);
-            });
-        })
-        ->when($regionVin !== null, function ($query) use ($regionVin) {
-            $query->whereHas('bouteille', function ($subquery) use ($regionVin) {
-                $subquery->where('region', 'like', "$regionVin%");
-            });
-        })
-        ->when($imageVin !== null, function ($query) use ($imageVin) {
-            if ($imageVin === '1') {
-                $query->whereHas('bouteille', function ($subquery) {
-                    $subquery->whereNotNull('srcImage');
+            })
+            ->when($typeVin, function ($query) use ($typeVin) {
+                if ($typeVin !== '') {
+                    $query->whereHas('bouteille', function ($subquery) use ($typeVin) {
+                        $subquery->where('type', 'like', "%$typeVin%");
+                    });
+                }
+            })
+            ->when($anneeVin !== null, function ($query) use ($anneeVin) {
+                $query->whereHas('bouteille', function ($subquery) use ($anneeVin) {
+                    $subquery->where('annee', $anneeVin);
                 });
-            } elseif ($imageVin === '0') {
-                $query->whereHas('bouteille', function ($subquery) {
-                    $subquery->whereNull('srcImage');
+            })
+            ->when($paysVin !== null, function ($query) use ($paysVin) {
+                $query->whereHas('bouteille', function ($subquery) use ($paysVin) {
+                    $subquery->where('pays', $paysVin);
                 });
-            }
-        })
-        ->when($prixMin !== null, function ($query) use ($prixMin) {
-            $query->whereHas('bouteille', function ($subquery) use ($prixMin) {
-                $subquery->where('prix', '>=', $prixMin);
+            })
+            ->when($regionVin !== null, function ($query) use ($regionVin) {
+                $query->whereHas('bouteille', function ($subquery) use ($regionVin) {
+                    $subquery->where('region', 'like', "$regionVin%");
+                });
+            })
+            ->when($imageVin !== null, function ($query) use ($imageVin) {
+                if ($imageVin === '1') {
+                    $query->whereHas('bouteille', function ($subquery) {
+                        $subquery->whereNotNull('srcImage');
+                    });
+                } elseif ($imageVin === '0') {
+                    $query->whereHas('bouteille', function ($subquery) {
+                        $subquery->whereNull('srcImage');
+                    });
+                }
+            })
+            ->when($prixMin !== null, function ($query) use ($prixMin) {
+                $query->whereHas('bouteille', function ($subquery) use ($prixMin) {
+                    $subquery->where('prix', '>=', $prixMin);
+                });
+            })
+            ->when($prixMax !== null, function ($query) use ($prixMax) {
+                $query->whereHas('bouteille', function ($subquery) use ($prixMax) {
+                    $subquery->where('prix', '<=', $prixMax);
+                });
+            })
+            ->when($tauxSucre !== null, function ($query) use ($tauxSucre) {
+                $query->whereHas('bouteille', function ($subquery) use ($tauxSucre) {
+                    $subquery->where('tauxSucre', $tauxSucre);
+                });
+            })
+            ->when($degre !== null, function ($query) use ($degre) {
+                $query->whereHas('bouteille', function ($subquery) use ($degre) {
+                    $subquery->where('degre', $degre);
+                });
+            })
+            ->when($sort, function ($query) use ($sort) {
+                if ($sort == 'name-asc') {
+                    $query->orderBy('bouteille.nom');
+                } elseif ($sort == 'name-desc') {
+                    $query->orderByDesc('bouteille.nom');
+                } elseif ($sort == 'price-asc') {
+                    $query->orderBy('bouteille.prix');
+                } elseif ($sort == 'price-desc') {
+                    $query->orderByDesc('bouteille.prix');
+                }
             });
-        })
-        ->when($prixMax !== null, function ($query) use ($prixMax) {
-            $query->whereHas('bouteille', function ($subquery) use ($prixMax) {
-                $subquery->where('prix', '<=', $prixMax);
-            });
-        })
-        ->when($tauxSucre !== null, function ($query) use ($tauxSucre) {
-            $query->whereHas('bouteille', function ($subquery) use ($tauxSucre) {
-                $subquery->where('tauxSucre', $tauxSucre);
-            });
-        })
-        ->when($degre !== null, function ($query) use ($degre) {
-            $query->whereHas('bouteille', function ($subquery) use ($degre) {
-                $subquery->where('degre', $degre);
-            });
-        })
-        ->when($sort, function ($query) use ($sort) {
-            if ($sort == 'name-asc') {
-                $query->orderBy('bouteille.nom');
-            } elseif ($sort == 'name-desc') {
-                $query->orderByDesc('bouteille.nom');
-            } elseif ($sort == 'price-asc') {
-                $query->orderBy('bouteille.prix');
-            } elseif ($sort == 'price-desc') {
-                $query->orderByDesc('bouteille.prix');
-            }
-        });
-
-    $bouteilles = $bouteilleQuery->get();
-
-    return view('cellier.show-search', compact('cellier', 'bouteilles'));
-}
+    
+        $bouteilles = $bouteilleQuery->get();
+    
+        return view('cellier.show-search', compact('cellier', 'bouteilles'));
+    }
+    
 }
--- resources/views/cellier/recherche.blade.php
+++ resources/views/cellier/recherche.blade.php
@@ -25,6 +25,7 @@
 
         <form action="{{ route('rechercheEtFiltrage.cellier', ['cellier_id' => $cellier->id]) }}" method="post" class="form-inline">
             @csrf
+
             <label for="keyword" class="sr-only">Recherche par mot-clé, phrase ou nom de bouteille :</label>
             <input type="text" name="keyword" id="keyword" class="form-control mb-2 mr-sm-2" placeholder="Recherche par mot-clé, phrase ou nom de bouteille" value="{{ request('keyword') }}">
 
@@ -80,40 +81,39 @@
                 <label for="degre" class="sr-only">Degré d'alcool :</label>
                 <input type="text" name="degre" id="degre" class="form-control mb-2 mr-sm-2" placeholder="Degré d'alcool" value="{{ request('degre') }}">
             </div>
-           
-            <button type="button" class="btn btn-primary mb-2 mr-sm-2" data-toggle="collapse" data-target="#collapseFiltrage" aria-expanded="false" aria-controls="collapseFiltrage">
-                Filtrage
+
+            <button type="submit" id="btnFiltrage" class="btn btn-primary mb-2 mr-sm-2" data-toggle="collapse" data-target="#collapseFiltrage" aria-expanded="false" aria-controls="collapseFiltrage">
+                             Filtrage
             </button>
 
-            <div class="collapse" id="collapseFiltrage">
-            </div>
 
             <button type="submit" class="btn btn-primary mb-2">Rechercher</button>
         </form>
+        <!-- Afficher les résultats de la recherche -->
         @if(isset($bouteilles) && $bouteilles->isNotEmpty())
-       <div class="search-card-container">
-            @foreach ($bouteilles as $bouteille)
-              <div class="search-card">
-                  @if ($bouteille->bouteille->srcImage || $bouteille->bouteille->srcsetImage)
-                    @php
-                        $imagePath = $bouteille->bouteille->srcImage ? $bouteille->bouteille->srcImage : $bouteille->bouteille->srcsetImage;
-                        $imagePath = Str::startsWith($imagePath, 'http') ? $imagePath : asset('images/' . $imagePath);
-                    @endphp
-                    <img src="{{ $imagePath }}" alt="Image de la bouteille">
-                @else
-                    <span>Aucune image disponible</span>
-                @endif
+            <div class="search-card-container">
+                @foreach ($bouteilles as $bouteille)
+                    <div class="search-card">
+                        @if ($bouteille->bouteille->srcImage || $bouteille->bouteille->srcsetImage)
+                            @php
+                                $imagePath = $bouteille->bouteille->srcImage ? $bouteille->bouteille->srcImage : $bouteille->bouteille->srcsetImage;
+                                $imagePath = Str::startsWith($imagePath, 'http') ? $imagePath : asset('images/' . $imagePath);
+                            @endphp
+                            <img src="{{ $imagePath }}" alt="Image de la bouteille">
+                        @else
+                            <span>Aucune image disponible</span>
+                        @endif
 
-                <h3>{{ $bouteille->bouteille->nom }}</h3>
-                <p>Type: {{ $bouteille->bouteille->type }}</p>
-                <p>Région: {{ $bouteille->bouteille->region }}</p>
-                <p>Année: {{ $bouteille->bouteille->annee }}</p>
-              </div>
-           @endforeach
-      </div>
-@else
-    <p>Aucun résultat trouvé.</p>
-@endif
+                        <h3>{{ $bouteille->bouteille->nom }}</h3>
+                        <p>Type: {{ $bouteille->bouteille->type }}</p>
+                        <p>Région: {{ $bouteille->bouteille->region }}</p>
+                        <p>Année: {{ $bouteille->bouteille->annee }}</p>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <p>Aucun résultat trouvé.</p>
+        @endif
 
         @if(!isset($cellier) && (!isset($bouteilles) || $bouteilles->isEmpty()))
             <a href="{{ route('cellier.create') }}" class="btn btn-success">
@@ -127,8 +127,21 @@
             <a href="{{ route('cellier.index') }}">Retour à la liste des celliers</a>
         @endif
 
+         <!-- Voir les bouteilles dans le cellier -->
         @if(isset($cellierId) && $cellierId)
             <a href="{{ route('bouteille.index', ['cellier_id' => $cellierId]) }}">Voir les bouteilles</a>
         @endif
+
+         <!--  Script pour le bouton de filtrage  -->
+        <script>
+           document.getElementById('btnFiltrage').addEventListener('click', function() {
+                var collapseFiltrage = document.getElementById('collapseFiltrage');
+                if (collapseFiltrage.classList.contains('show')) {
+                    collapseFiltrage.classList.remove('show');
+                } else {
+                    collapseFiltrage.classList.add('show');
+                }
+            });
+        </script>
     </main>
 @endsection
